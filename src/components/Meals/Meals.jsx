8efff3f257b9971cf4e9e52b5662121055039b4c
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 12:14:22 +0300

    Imports react components to meals

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import "./Meals.css";
+import Meal from "../Meal/Meal";
+import MealContext from "../../context/MealContext";
+import Spinner from "../Spinner/Spinner";
