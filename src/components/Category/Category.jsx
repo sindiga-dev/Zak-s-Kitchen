b3a36536add9e3a5652f667f933c133ddf33ba0e
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 10:57:53 +0300

    Imports components to category.jsx

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Category.css';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Checkbox from '@material-ui/core/Checkbox';
+import Avatar from '@material-ui/core/Avatar';
+
+import MealContext from "../../context/MealContext";
