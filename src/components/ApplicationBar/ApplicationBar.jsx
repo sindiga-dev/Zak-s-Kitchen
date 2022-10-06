0118ce9a9e1dba386727a9a3a01205d91e151b88
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 10:56:41 +0300

    Renders application bar

--- src/components/ApplicationBar/ApplicationBar.jsx
+++ src/components/ApplicationBar/ApplicationBar.jsx
@@ -8,3 +8,47 @@ import Typography from '@material-ui/core/Typography';
 import Badge from '@material-ui/core/Badge';
 import { withStyles } from '@material-ui/core/styles';
 import MealContext from "../../context/MealContext";
+
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        cursor: 'pointer'
+    },
+    grow: {
+        flexGrow: 1,
+    }
+});
+
+class ApplicationBar extends Component {
+    handleLogo=()=>{
+        this.props.history.push('/meal-app');
+    }
+    render() {
+        const {classes}=this.props;
+        return (
+            <MealContext.Consumer>
+            {(context) => (
+              <div className={classes.root}>
+                <AppBar position="static">
+                  <Toolbar>
+                    <Typography onClick={this.handleLogo} className={classes.title} variant="h6" color="inherit" noWrap>Meal App</Typography>
+                    <div className={classes.grow} />
+                    <React.Fragment>
+                      <Link to="/fav">
+                        <IconButton color="inherit">
+                          <Badge badgeContent={context.favoriteMeals.length} showZero color="secondary">
+                            <i className="fa fa-heart"></i>
+                          </Badge>
+                        </IconButton>
+                      </Link>
+                    </React.Fragment>
+                  </Toolbar>
+                </AppBar>
+              </div>
+            )}
+          </MealContext.Consumer>
+        )
+      }
+    }
+export default withStyles(styles)(ApplicationBar);
