a1b6a70386330cb9178382769125f9bcbaa15eba
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 12:24:19 +0300

    Renders meals

--- src/components/Meals/Meals.jsx
+++ src/components/Meals/Meals.jsx
@@ -3,3 +3,29 @@ import "./Meals.css";
 import Meal from "../Meal/Meal";
 import MealContext from "../../context/MealContext";
 import Spinner from "../Spinner/Spinner";
+
+export default class Meals extends Component {
+    handleBackButton=()=>{
+        //meal app /
+        this.props.history.push('/meal-app');
+    };
+    renderMeals(context) {
+        let { meals, favoriteMeals } = context;
+        if (meals.length > 0) {
+          return (
+            <div className="meals-container">
+              {meals.map(meal => (
+                <React.Fragment key={meal.idMeal}>
+                  <Meal
+                    meal={meal}
+                    favoriteMeals={favoriteMeals}
+                    context={context}
+                  />
+                </React.Fragment>
+              ))}
+            </div>
+          );
+        } else {
+          return <Spinner />;
+        }
+      }
