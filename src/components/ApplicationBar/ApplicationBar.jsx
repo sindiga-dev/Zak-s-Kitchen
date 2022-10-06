f6dddaa850297eb7d767a3c22d5cffae499d4b3a
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 10:29:51 +0300

    Imports items to Application bar

---
+++ src/components/ApplicationBar/ApplicationBar.jsx
@@ -0,0 +1,10 @@
+//Application bar
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Badge from '@material-ui/core/Badge';
+import { withStyles } from '@material-ui/core/styles';
+import MealContext from "../../context/MealContext";
